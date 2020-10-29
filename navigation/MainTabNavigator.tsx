36e018d132f124998a8e58fdce0f07a6e1caa224
Aniket kumar sharma <sharma.ani.4298@gmail.com>
2020-10-29 11:48:42 +0530

    header done

---
+++ navigation/MainTabNavigator.tsx
@@ -0,0 +1,89 @@
+import { Ionicons } from "@expo/vector-icons";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as React from "react";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import Colors from "../constants/Colors";
+import useColorScheme from "../hooks/useColorScheme";
+import TabOneScreen from "../screens/TabOneScreen";
+import TabTwoScreen from "../screens/TabTwoScreen";
+import { MainTabParamList, TabOneParamList, TabTwoParamList } from "../types";
+
+const MainTab = createMaterialTopTabNavigator<MainTabParamList>();
+
+export default function MainTabNavigator() {
+    const colorScheme = useColorScheme();
+
+    return (
+        <MainTab.Navigator
+            initialRouteName="TabOne"
+            tabBarOptions={{
+                activeTintColor: Colors[colorScheme].background,
+                style: {
+                    backgroundColor: Colors[colorScheme].tint,
+                },
+                indicatorStyle: {
+                    backgroundColor: Colors[colorScheme].background,
+                    height: 3,
+                },
+                labelStyle: {
+                    fontWeight: "bold",
+                },
+            }}
+        >
+            <MainTab.Screen
+                name="TabOne"
+                component={TabOneNavigator}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <TabBarIcon name="ios-code" color={color} />
+                    ),
+                }}
+            />
+            <MainTab.Screen
+                name="TabTwo"
+                component={TabTwoNavigator}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <TabBarIcon name="ios-code" color={color} />
+                    ),
+                }}
+            />
+        </MainTab.Navigator>
+    );
+}
+
+// You can explore the built-in icon families and icons on the web at:
+// https://icons.expo.fyi/
+function TabBarIcon(props: { name: string; color: string }) {
+    return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
+}
+
+// Each tab has its own navigation stack, you can read more about this pattern here:
+// https://reactnavigation.org/docs/tab-based-navigation#a-stack-navigator-for-each-tab
+const TabOneStack = createStackNavigator<TabOneParamList>();
+
+function TabOneNavigator() {
+    return (
+        <TabOneStack.Navigator>
+            <TabOneStack.Screen
+                name="TabOneScreen"
+                component={TabOneScreen}
+                options={{ headerTitle: "Tab One Title" }}
+            />
+        </TabOneStack.Navigator>
+    );
+}
+
+const TabTwoStack = createStackNavigator<TabTwoParamList>();
+
+function TabTwoNavigator() {
+    return (
+        <TabTwoStack.Navigator>
+            <TabTwoStack.Screen
+                name="TabTwoScreen"
+                component={TabTwoScreen}
+                options={{ headerTitle: "Tab Two Title" }}
+            />
+        </TabTwoStack.Navigator>
+    );
+}
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,23 +1,35 @@
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
-import { ColorSchemeName } from 'react-native';
+import { Entypo, Octicons } from "@expo/vector-icons";
+import {
+    NavigationContainer,
+    DefaultTheme,
+    DarkTheme,
+} from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as React from "react";
+import { ColorSchemeName, TouchableOpacity } from "react-native";
+import { View } from "../components/Themed";
+import Colors from "../constants/Colors";
 
-import NotFoundScreen from '../screens/NotFoundScreen';
-import { RootStackParamList } from '../types';
-import BottomTabNavigator from './BottomTabNavigator';
-import LinkingConfiguration from './LinkingConfiguration';
+import NotFoundScreen from "../screens/NotFoundScreen";
+import { RootStackParamList } from "../types";
+import BottomTabNavigator from "./MainTabNavigator";
+import LinkingConfiguration from "./LinkingConfiguration";
 
 // If you are not familiar with React Navigation, we recommend going through the
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
-export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
-  return (
-    <NavigationContainer
-      linking={LinkingConfiguration}
-      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <RootNavigator />
-    </NavigationContainer>
-  );
+export default function Navigation({
+    colorScheme,
+}: {
+    colorScheme: ColorSchemeName;
+}) {
+    return (
+        <NavigationContainer
+            linking={LinkingConfiguration}
+            theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+        >
+            <RootNavigator />
+        </NavigationContainer>
+    );
 }
 
 // A root stack navigator is often used for displaying modals on top of all other content
@@ -25,10 +37,59 @@ export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeNa
 const Stack = createStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
-  return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Root" component={BottomTabNavigator} />
-      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
-    </Stack.Navigator>
-  );
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: Colors.light.tint,
+                    shadowOpacity: 0,
+                    elevation: 0,
+                },
+                headerTitleStyle: {
+                    color: Colors.light.background,
+                    fontWeight: "bold",
+                    textAlign: "left",
+                },
+            }}
+        >
+            <Stack.Screen
+                name="Root"
+                component={BottomTabNavigator}
+                options={{
+                    title: "WhatsApp",
+
+                    headerRight: () => (
+                        <View
+                            style={{
+                                flexDirection: "row",
+                                backgroundColor: Colors.light.tint,
+                                width: 80,
+                                justifyContent: "space-around",
+                            }}
+                        >
+                            <TouchableOpacity activeOpacity={0.8}>
+                                <Octicons
+                                    name="search"
+                                    size={22}
+                                    color={Colors.light.background}
+                                />
+                            </TouchableOpacity>
+                            <TouchableOpacity activeOpacity={0.8}>
+                                <Entypo
+                                    name="dots-three-vertical"
+                                    size={22}
+                                    color={Colors.light.background}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                    ),
+                }}
+            />
+            <Stack.Screen
+                name="NotFound"
+                component={NotFoundScreen}
+                options={{ title: "Oops!" }}
+            />
+        </Stack.Navigator>
+    );
 }
